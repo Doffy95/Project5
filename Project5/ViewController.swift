30c07aefaa98de5162ee3b438141ece6b895f8b3
zug <dungnguyen.ttcb@gmail.com>
2022-09-30 23:44:35 +0700

    Project have done. Completed challenges

--- Project5/ViewController.swift
+++ Project5/ViewController.swift
@@ -10,13 +10,15 @@ import UIKit
 class ViewController: UITableViewController {
     
     var allWords: [String] = []
-    var useWord: [String] = []
-
+    var useWords: [String] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
         navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(promptForAnswer))
+        
+        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .refresh, target: self, action: #selector(restartGame))
         
         if let startWordsURL = Bundle.main.url(forResource: "start", withExtension: "txt") {
             if let startWords = try? String(contentsOf: startWordsURL) {
@@ -30,15 +32,99 @@ class ViewController: UITableViewController {
         
         startGame()
     }
-
+    
     func startGame() {
         title = allWords.randomElement()
-        useWord.removeAll(keepingCapacity: true)
+        useWords.removeAll(keepingCapacity: true)
         tableView.reloadData()
     }
     
-    func submit(_ answer: String) {
+    func isPossible(word: String) -> Bool {
+        guard var tempWord = title?.lowercased() else { return false }
         
+        if word.lowercased() == tempWord {
+            return false
+        }
+        
+        for letter in word {
+            if let position = tempWord.firstIndex(of: letter) {
+                tempWord.remove(at: position)
+            } else {
+                return false
+            }
+        }
+        
+        return true
+    }
+    
+    func isOriginal(word: String) -> Bool {
+        return !useWords.contains(word)
+    }
+    
+    func isReal(word: String) -> Bool {
+        if word.count < 3 {
+            return false
+        }
+        
+        let checker = UITextChecker()
+        let range = NSRange(location: 0, length: word.utf16.count)
+        let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
+        
+        return misspelledRange.location == NSNotFound
+    }
+    
+    func submit(answer: String) {
+        let lowerAnswer = answer.lowercased()
+        
+        if isPossible(word: lowerAnswer) {
+            if isOriginal(word: lowerAnswer) {
+                if isReal(word: lowerAnswer) {
+                    useWords.insert(answer, at: 0)
+                    
+                    let indexPath = IndexPath(row: 0, section: 0)
+                    tableView.insertRows(at: [indexPath], with: .automatic)
+                    
+                    return
+                } else {
+                    showErrorMessage(funcErr: "isReal")
+                }
+            } else {
+                showErrorMessage(funcErr: "isOriginal")
+            }
+        } else {
+            showErrorMessage(funcErr: "isPossible")
+        }
+    }
+    
+    func showErrorMessage(funcErr: String) {
+        let errorTitle: String
+        let errorMessage: String
+        
+        switch funcErr {
+        case "isPossible":
+            errorTitle = "Word not recognised"
+            errorMessage = "You can't just make them up, you know!"
+        case "isOriginal":
+            errorTitle = "Word used already"
+            errorMessage = "Be more original!"
+        case "isReal":
+            guard let title = title?.lowercased() else { return }
+            errorTitle = "Word not possible"
+            errorMessage = "You can't spell that word from \(title)"
+        default:
+            errorTitle = "Unknown error"
+            errorMessage = "Unknown error"
+        }
+        
+        let ac = UIAlertController(title: errorTitle, message: errorMessage, preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "OK", style: .default))
+        present(ac, animated: true)
+    }
+    
+    @objc func restartGame() {
+        useWords.removeAll(keepingCapacity: false)
+        title = allWords.randomElement()
+        tableView.reloadData()
     }
     
     @objc func promptForAnswer() {
@@ -47,7 +133,7 @@ class ViewController: UITableViewController {
         
         let submitAction = UIAlertAction(title: "Submit", style: .default) { [weak self, weak ac] action in
             guard let answer = ac?.textFields?[0].text else { return }
-            self?.submit(answer)
+            self?.submit(answer: answer)
         }
         
         ac.addAction(submitAction)
@@ -55,12 +141,12 @@ class ViewController: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return allWords.count
+        return useWords.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Word", for: indexPath)
-        cell.textLabel?.text = allWords[indexPath.row]
+        cell.textLabel?.text = useWords[indexPath.row]
         return cell
     }
 }
